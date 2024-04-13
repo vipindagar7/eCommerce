3d066397c0518a619f5d172f7f9b7a7298575cb6
Vipin Dagar <dagarv23@gmail.com>
2024-04-13 22:29:28 +0530

    Update settings.py

--- thriftedTrends/settings.py
+++ thriftedTrends/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'codewithvipin@gmail.com'
-EMAIL_HOST_PASSWORD = 'rctg ydly mvte bgrv'
+EMAIL_HOST_USER = 'ENTER_HOST_USER'
+EMAIL_HOST_PASSWORD = 'ENTER_HOST_PASSWORD'
